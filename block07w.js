c659dbe5e46bec1a204ca9ca800bd9bf35086c5a
Tristan <tristanscotttanderson@gmail.com>
2023-10-05 13:05:34 -0500

    final commit

---
+++ block07w.js
@@ -0,0 +1,5 @@
+// Pseudocode: read the requirements, perform one requirement at a time, ask questions when needed.
+
+let string1 = "You have received this message because you have been chosen to open an important vault.here is the secret combination"
+
+console.log(string1)
