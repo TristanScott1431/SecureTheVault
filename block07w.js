e1bc90d67d828c545002f1bf7e96f472a76704b5
Tristan <tristanscotttanderson@gmail.com>
2023-10-05 13:16:30 -0500

    final commit

--- block07w.js
+++ block07w.js
@@ -1,5 +1,22 @@
-// Pseudocode: read the requirements, perform one requirement at a time, ask questions when needed.
+// Pseudocode: read the requirements, perform one requirement at a time, ask questions when needed, finish and turn in through github.
 
-let string1 = "You have received this message because you have been chosen to open an important vault.here is the secret combination"
+//create string that requires specific comment//
+let string1 = "You have received this message because you have been chosen to open an important vault. Here is the secret combination"
 
 console.log(string1)
+
+//create variable that equals 10//
+let var1 = 20 / 2
+
+console.log(var1)
+
+//create variable that equals 40//
+let var2 = 5 * 8
+
+console.log(var2)
+
+//create variable that equals 39//
+let var3 = -1 + 40
+
+console.log(var3)
+
